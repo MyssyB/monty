cc72d1b90d1f56a2488b42977bf8995350c123fb
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-01-18 10:27:20 +0000

    Add monty.c file

---
+++ monty.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "monty.h"
+
+/**
+  * main - entry point of the program
+  * @argc: number or arguments
+  * @argv: argument list
+  *
+  * Return: 0 on success otherwise none zero
+  */
+int main(int argc, char **argv)
+{
+	printf("Monty bytecode :)\n");
+	return (0);
+}
